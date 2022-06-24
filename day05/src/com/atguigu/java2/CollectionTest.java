8b14161d94763bc841229de865cbe007b555b695
ZhangWeichen <734676881@qq.com>
2022-06-24 17:30:59 +0800

    keep learning

--- day05/src/com/atguigu/java2/CollectionTest.java
+++ day05/src/com/atguigu/java2/CollectionTest.java
@@ -1,9 +1,15 @@
 package com.atguigu.java2;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * 一、集合框架的概述
  *
- * 1.集合、数组都是对多个数据进行存储操作的结构，简称Java容器。
+ * 1.集合、数组都是对多个数据进行存储操作的结构，简称 Java容器。
  *   说明：此时的存储，主要指的是内存层面的存储，不涉及到持久化的存储（.txt,.jpg,.avi，数据库中）
  *
  * 2.1 数组在存储多个数据方面的特点：
@@ -35,4 +41,34 @@ package com.atguigu.java2;
  * @create 2022-05-18 23:36
  */
 public class CollectionTest {
+
+    @Test
+    public void test1() {
+        Collection coll = new ArrayList();
+
+        // add(Object e)：将元素e添加到集合coll中
+        coll.add("aa");
+        coll.add("BB");
+        coll.add(123);  // 自动装箱生成包装类对象
+        coll.add(new Date());
+
+        // size()：获取集合中的元素数量
+        System.out.println(coll.size());  // 4
+
+        // addAll(Collection coll1)：将coll1集合中的元素添加到当前集合中
+        Collection coll1 = new ArrayList();
+        coll1.add("cc");
+        coll1.add(456);
+
+        coll.addAll(coll1);
+
+        System.out.println(coll.size());  // 6
+        System.out.println(coll);
+
+        // clear()：清空集合元素
+        coll.clear();
+
+        // isEmpty()：判断是否为空集合
+        System.out.println(coll.isEmpty());  // true
+    }
 }
