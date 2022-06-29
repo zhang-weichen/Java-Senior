885c613e568c89d58fd6552fd834f124d7707c91
ZhangWeichen <734676881@qq.com>
2022-06-29 17:30:29 +0800

    keep learning

--- day06/src/com/atguigu/java/ListTest.java
+++ day06/src/com/atguigu/java/ListTest.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 1. List接口框架
  *
  *    |----Collection接口：单列集合，用来存储一个一个的对象
- *          |----List接口：存储有序的、可重复的数据。  -->“动态”数组,替换原有的数组
+ *          |----List接口：存储有序的、可重复的数据。  --> “动态”数组,替换原有的数组
  *              |----ArrayList：作为List接口的主要实现类；线程不安全的，效率高；底层使用Object[] elementData存储
  *              |----LinkedList：对于频繁的插入、删除操作，使用此类效率比ArrayList高；底层使用双向链表存储
  *              |----Vector：作为List接口的古老实现类；线程安全的，效率低；底层使用Object[] elementData存储
