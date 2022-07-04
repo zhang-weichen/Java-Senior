80a15a0985cb66caba2eb6f71c545e66bc84ef1f
zhangweichen <734676881@qq.com>
2022-07-05 00:18:45 +0800

    keep learning

--- day02/src/com/atguigu/java2/CommunicationTest.java
+++ day02/src/com/atguigu/java2/CommunicationTest.java
@@ -1,18 +1,18 @@
 package com.atguigu.java2;
 
 /**
- * 线程通信的例子：使用两个线程打印 1-100。线程1、线程2 交替打印
+ * 线程通信的例子：使用两个线程打印 1-100。线程 1、线程 2 交替打印
  *
  * 线程通信涉及的三个方法：
  * wait()：一旦执行此方法，当前线程就进入阻塞状态，并释放同步监视器。
- * notify()：一旦执行此方法，就会唤醒被wait的一个线程。如果有多个线程被wait，就唤醒优先级最高的那个。
- * notifyAll()：一旦执行此方法，就会唤醒所有被wait的方法。
+ * notify()：一旦执行此方法，就会唤醒被 wait的一个线程。如果有多个线程被 wait，就唤醒优先级最高的那个。
+ * notifyAll()：一旦执行此方法，就会唤醒所有被 wait的方法。
  *
  * 说明：
  * 1. wait()、notify()、notifyAll()三个方法必须在同步代码块或同步方法中使用。
  * 2. wait()、notify()、notifyAll()三个方法的调用者必须是同步代码块或同步方法中的同步监视器。
- *    否则会出现IllegalMonitorStateException。
- * 3. wait()、notify()、notifyAll()三个方法定义在java.lang.Object类中。
+ *    否则会出现 IllegalMonitorStateException。
+ * 3. wait()、notify()、notifyAll()三个方法定义在 java.lang.Object类中。
  *
  * 面试题：sleep() 和 wait() 的异同？
  * 1. 相同点：一旦执行方法，都可以使当前线程进入阻塞状态。
@@ -33,7 +33,7 @@ class Number implements Runnable {
 
         while (true) {
 
-//            synchronized (obj) {  // 报错，同步监视器与notify()和wait()的调用者不一致
+//            synchronized (obj) {  // 报错，同步监视器与 notify()和 wait()的调用者不一致
             synchronized (this) {
 
                 this.notify();
@@ -50,8 +50,8 @@ class Number implements Runnable {
                     number++;
 
                     try {
-                        // 使得调用如下wait()方法的线程进入阻塞状态
-                        // 调用wait()方法的线程会释放锁
+                        // 使得调用如下 wait()方法的线程进入阻塞状态
+                        // 调用 wait()方法的线程会释放锁
                         wait();
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
